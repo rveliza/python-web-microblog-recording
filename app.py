402c2ece0115b786d48cc2f0aff96ea4f4101663
Reyner Veliz <reyner@reynerveliz.com>
2024-03-08 15:22:13 -0600

    Display past entries in Microblog project

--- app.py
+++ app.py
@@ -3,14 +3,15 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+entries = []
 
 @app.route('/', methods=["GET", "POST"])
 def home():
     if request.method == "POST":
         entry_content = request.form.get("content")
         formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
-        print(entry_content, formatted_date)
-    return render_template('home.html')
+        entries.append((entry_content, formatted_date))
+    return render_template('home.html', entries=entries)
 
 if __name__ == "__main__":
     app.run(debug=True)
