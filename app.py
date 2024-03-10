2f602188f8c63797afc391df764d092317a6629c
Reyner Veliz <reyner@reynerveliz.com>
2024-03-10 01:30:43 -0600

    Connect to Mongodb and send form data to db

--- app.py
+++ app.py
@@ -1,17 +1,25 @@
 import datetime
 from flask import Flask, render_template, request
+from pymongo import MongoClient
 
 app = Flask(__name__)
+client = MongoClient("mongodb+srv://reyner:WIFUp3FzIiFS6AE3@microblogcluster.eoxsbey.mongodb.net/")
+app.db = client.microblog
 
 entries = []
 
 @app.route('/', methods=["GET", "POST"])
 def home():
+    # new_entries = app.db.entries.find({})
+    # print([e for e in new_entries])
     if request.method == "POST":
         entry_content = request.form.get("content")
         formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
         short_date = datetime.datetime.today().strftime("%b %d")
         entries.append((entry_content, formatted_date, short_date))
+        app.db.entries.insert_one({
+            "content": entry_content, 
+            "date": formatted_date})
 
     # entries_with_date = [(
     #     entry[0],
