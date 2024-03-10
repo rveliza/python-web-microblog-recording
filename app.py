f768c1cf264b693527184648e834dc82b213dd4e
Reyner Veliz <reyner@reynerveliz.com>
2024-03-10 00:44:16 -0600

    Add a new date format

--- app.py
+++ app.py
@@ -10,8 +10,18 @@ def home():
     if request.method == "POST":
         entry_content = request.form.get("content")
         formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
-        entries.append((entry_content, formatted_date))
+        short_date = datetime.datetime.today().strftime("%b %d")
+        entries.append((entry_content, formatted_date, short_date))
+
+    # entries_with_date = [(
+    #     entry[0],
+    #     entry[1],
+    #     datetime.datetime.strptime(entry[1], "%Y-%m-%d").strftime('%b %d')) 
+    #     for entry in entries]
+    
+
     return render_template('home.html', entries=entries)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
