00c858ba34aaa64cce4c1672c184280f8fd43588
Reyner Veliz <reyner@reynerveliz.com>
2024-03-10 14:31:00 -0600

    Finished project

--- app.py
+++ app.py
@@ -2,30 +2,29 @@ import datetime
 from flask import Flask, render_template, request
 from pymongo import MongoClient
 
-app = Flask(__name__)
-client = MongoClient("mongodb+srv://reyner:WIFUp3FzIiFS6AE3@microblogcluster.eoxsbey.mongodb.net/")
-app.db = client.microblog
+
+def create_app():
+    app = Flask(__name__)
+    client = MongoClient("mongodb+srv://reyner:WIFUp3FzIiFS6AE3@microblogcluster.eoxsbey.mongodb.net/")
+    app.db = client.microblog
 
 
-@app.route('/', methods=["GET", "POST"])
-def home():
-    entries = [(
-        entry["content"],
-        entry["date"],
-        entry["short_date"]
-    ) for entry in app.db.entries.find({})]
- 
-    if request.method == "POST":
-        entry_content = request.form.get("content")
-        formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
-        short_date = datetime.datetime.today().strftime("%b %d")
-        entries.append((entry_content, formatted_date, short_date))
-        app.db.entries.insert_one({
-            "content": entry_content, 
-            "date": formatted_date,
-            "short_date": short_date})
-    return render_template('home.html', entries=entries)
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
+    @app.route('/', methods=["GET", "POST"])
+    def home():
+        entries = [(
+            entry["content"],
+            entry["date"],
+            entry["short_date"]
+        ) for entry in app.db.entries.find({})]
+    
+        if request.method == "POST":
+            entry_content = request.form.get("content")
+            formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
+            short_date = datetime.datetime.today().strftime("%b %d")
+            entries.append((entry_content, formatted_date, short_date))
+            app.db.entries.insert_one({
+                "content": entry_content, 
+                "date": formatted_date,
+                "short_date": short_date})
+        return render_template('home.html', entries=entries)
+    return app
