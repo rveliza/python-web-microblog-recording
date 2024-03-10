23c6d7018fda375ea76e31288d0cbefe9b8cc9ed
Reyner Veliz <reyner@reynerveliz.com>
2024-03-10 01:48:31 -0600

    Retrieve data from mongodb

--- app.py
+++ app.py
@@ -6,12 +6,15 @@ app = Flask(__name__)
 client = MongoClient("mongodb+srv://reyner:WIFUp3FzIiFS6AE3@microblogcluster.eoxsbey.mongodb.net/")
 app.db = client.microblog
 
-entries = []
 
 @app.route('/', methods=["GET", "POST"])
 def home():
-    # new_entries = app.db.entries.find({})
-    # print([e for e in new_entries])
+    entries = [(
+        entry["content"],
+        entry["date"],
+        entry["short_date"]
+    ) for entry in app.db.entries.find({})]
+ 
     if request.method == "POST":
         entry_content = request.form.get("content")
         formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
@@ -19,15 +22,8 @@ def home():
         entries.append((entry_content, formatted_date, short_date))
         app.db.entries.insert_one({
             "content": entry_content, 
-            "date": formatted_date})
-
-    # entries_with_date = [(
-    #     entry[0],
-    #     entry[1],
-    #     datetime.datetime.strptime(entry[1], "%Y-%m-%d").strftime('%b %d')) 
-    #     for entry in entries]
-    
-
+            "date": formatted_date,
+            "short_date": short_date})
     return render_template('home.html', entries=entries)
 
 
