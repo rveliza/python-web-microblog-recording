8f8922aa33561a1ce8cad3384b19e865a13d0818
Reyner Veliz <reyner@reynerveliz.com>
2024-03-10 19:00:44 -0600

    Create requirements.txt and .env variables

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-import datetime
+import datetime, os
 from flask import Flask, render_template, request
 from pymongo import MongoClient
+from dotenv import load_dotenv
 
+load_dotenv()
 
 def create_app():
     app = Flask(__name__)
-    client = MongoClient("mongodb+srv://reyner:WIFUp3FzIiFS6AE3@microblogcluster.eoxsbey.mongodb.net/")
+    client = MongoClient(os.getenv("MONGODB_URI"))
     app.db = client.microblog
 
 
